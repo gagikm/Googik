e0fbcca633747594cb4d52616056c34e309baf9f
Gagik Movsisyan <gagikm@ucla.edu>
2015-07-23 09:06:07 -0700

    better footer

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ keyboardEvent[initMethod](
 );
     
 //         document.getElementById("search").focus();
-        document.getElementById("search").value = "|";
+        document.getElementById("search").value = " ";
 //        document.dispatchEvent(keyboardEvent);
 //    if(window.length>500)
          document.getElementById("search").click();
@@ -34,39 +34,33 @@ keyboardEvent[initMethod](
 
 function f(id){
     var val = document.getElementById("search").value;
-    switch(val.toLowerCase()) {
+    lower = val.toLowerCase();
+    trimmed = lower.trim();
+    switch(trimmed) {
     case "linkedin":
-    case "|linkedin":
             window.location.href = 'http://linkedin.com/in/gagikm';
             break;
     case "resume":  
-    case "|resume":  
         document.getElementById("search").value = "Press Enter to Download Resume [ PDF ]";
         break;
     case "press enter to download resume [ pdf ]":
         window.location.href = 'https://www.dropbox.com/s/jdyfqbb2v1wgz2t/updatedResume.pdf?dl=0';
         break;
     case "facebook":
-    case "|facebook":
         window.location.href = 'http://facebook.com/GagikMovsisyan';
         break;
     case "projects":
-    case "|projects":
     case "github": 
-    case "|github": 
         window.location.href = 'http://github.com/gagikm';
         break;
     case "twitter":
-    case "|twitter":
         window.location.href = 'http://twitter.com/gagikm';
         break;
     case "birthday":
-    case "|birthday":
         document.getElementsByTagName("body")[0].style = "background-image:url('img/birthday.png'); background-fit:contain";
         document.getElementById("search").value = "May 25";
         break;
     case "education": case "school": case "university": case "college": case "ucla":
-    case "|education": case "|school": case "|university": case "|college": case "|ucla":
         document.getElementsByTagName("body")[0].style = "background-image:url('img/ucla.png'); background-size:100%";
         document.getElementById("search").value = "University of California, Los Angeles";
         break;
@@ -74,31 +68,22 @@ function f(id){
         window.location.href = 'http://www.ucla.edu';
         break;
     case "clear":
-    case "|clear":
     case "reset":
-    case "|reset":
     case "clean":
-    case "|clean":
             document.getElementsByTagName("body")[0].style ="" ;
             break;
     case "name": 
-    case "|name": 
     case "full name": 
-    case "|full name": 
     case "gagik who": case "gagik who?":
-    case "|gagik who": case "|gagik who?":
             document.getElementById("search").value = "Gagik Movsisyan";
             break;
     case "email": case "send email": case "contact":
-    case "|email": case "|send email": case "|contact":
             window.location.href = 'mailto:gagikm@ucla.edu';
             break;
     case "clappy bear": case "clappybear":
-    case "|clappy bear": case "|clappybear":
             window.location.href = 'http://gagikm.github.io/clappybear';
             break;
     case "armateur": case "lahacks": case "leapmotion": case "leap motion":
-    case "|armateur": case "|lahacks": case "|leapmotion": case "|leap motion":
             window.location.href = 'https://www.leapmotion.com/blog/la-hack-finalists-armateur-robotic-arm-leap-motion-bluetooth/';
             break;
 
